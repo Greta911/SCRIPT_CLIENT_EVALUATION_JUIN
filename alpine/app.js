a6942f8e2acc77942d9beacee9e7c9414fa518d1
Greta911 <grema20012000@yahoo.it>
2026-05-25 11:50:14 +0200

    Function saveToStorage + addContact

--- alpine/app.js
+++ alpine/app.js
@@ -2,6 +2,36 @@
 
 function contactApp() {
     return {
-        contacts: JSON.parse(localStorage.contacts) || [],
+        contacts: JSON.parse(localStorage.getItem('contacts')) || [],
+    search: '',
+
+    //Champs pour le formulaire d'ajout
+    newFirstname: '',
+    newLastname: '',
+    newEmail: '',
+
+    //Ajout d'un contact
+    addContact() {
+        if (this.newFirstname.trim() === '' || this.newLastname.trim() === '' || this.newEmail.trim() === '') return;
+        this.contacts.push({
+            id: Date.now(),
+            firstname: this.newFirstname,
+            lastname: this.newLastname,
+            email: this.newEmail,
+            isEditing: false
+        });
+
+        //Réinitialiser les champs du formulaire
+        this.newFirstname = '';
+        this.newLastname = '';
+        this.newEmail = '';
+
+        //Sauvegarde dans le localStorage
+        this.saveToStorage();
+    },
+    saveToStorage() {
+        localStorage.contacts = JSON.stringify(this.contacts);
+    }
+
     };
 }
